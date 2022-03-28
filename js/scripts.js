72b3ccedf462b4731dd9b9286010284c98aaf6ce
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-03-27 17:56:04 -0700

    restart

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-const grid = document.querySelector(".grid");
-let html = '';
-
-for (let i=1; i<=12; i++) {
-  html += `<div class="item item-${i}"><img src="../images/thumbnails/${i}.jpg"></div>`;
-}
-
-grid.innerHTML = html;
-

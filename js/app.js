d48bcb5cc4ecdd4a224e02104e27735e9385f41d
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-03-29 17:23:47 -0700

    created own search filter, finalizations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 baguetteBox.run('.gallery');
 
-const search = new Filter('search', 'data-caption');
+function search() {
+  let input = document.querySelector('input').value.toLowerCase();
+  let images = document.querySelectorAll('a');
+
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].dataset.caption.toLowerCase().includes(input)) {
+      images[i].style.display = "block";
+    } else {
+      images[i].style.display = "none";
+    }
+  }
+}
